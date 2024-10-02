fe363a260c580136b6b8210ddb69e5e42ca2bf77
mkljla <shazhidaye8@gmail.com>
2024-10-02 10:21:32 +0900

    refactoring

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,14 @@ class SessionsController < ApplicationController
 
     # ログイン処理
     def create
-        user = User.find_by(employee_number: params[:employee_number].downcase)
-        if user && user.authenticate(params[:password])# パスワードの確認
+        user = User.find_by(employee_number: session_params[:employee_number].downcase)
+        if user && user.authenticate(session_params[:password])# パスワードの確認
             log_in user
             Rails.logger.info "Login successful for user: #{user.full_name} (Employee Number: #{user.employee_number})"
+            flash[:notice] = "ログインに成功しました"
             redirect_to user_home_path
         else
-            Rails.logger.warn "Login failed for employee number: #{params[:employee_number]}"
+            Rails.logger.warn "Login failed for employee number: #{session_params[:employee_number]}"
             flash.now[:alert] = "ログインに失敗しました。社員番号とパスワードを確認してください。"
             render 'new'
         end
@@ -22,5 +23,10 @@ class SessionsController < ApplicationController
         # ログアウト処理
         log_out if logged_in?
         redirect_to root_url
+    end
+
+    private
+    def session_params
+        params.require(:session).permit(:employee_number, :password)
     end
 end
