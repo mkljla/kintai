b58ab4ab77b591eb38ce62e50e4f7a7beaead67b
mkljla <shazhidaye8@gmail.com>
2024-11-07 12:28:53 +0900

    [update]トランザクション処理を追加

--- app/controllers/breaks_controller.rb
+++ app/controllers/breaks_controller.rb
@@ -21,14 +21,23 @@ class BreaksController < ApplicationController
     return redirect_with_alert("出勤していません。") unless @latest_work.working?
     # 休憩開始していない場合の処理を早期リターンで処理
     return redirect_with_alert("休憩を開始していません。") unless @latest_break.taking_a_break?
-    # 休憩終了処理を実行しリダイレクト
-    flash[:notice] = @latest_break.set_end_time ? "休憩を終了しました。" : "休憩時間の登録に失敗しました。"
 
-    total_break_time = @latest_break.calculate_total_break_time_in_minutes(@latest_break)
-    @latest_break.register_total_break_time_in_minutes(total_break_time)
+    ActiveRecord::Base.transaction do
+      # 休憩終了時間を登録
+      raise ActiveRecord::Rollback unless @latest_break.set_end_time
+      # 休憩時間を計算
+      total_break_time = @latest_break.calculate_total_break_time_in_minutes(@latest_break)
+      # 休憩時間を保存
+      raise ActiveRecord::Rollback unless @latest_break.register_total_break_time_in_minutes(total_break_time)
+    end
+
+    flash[:notice] = "休憩を終了しました。"
     redirect_to user_home_path
+
+    rescue StandardError => e
+      # 例外処理
+      flash[:alert] = "休憩の終了に失敗しました: #{e.message}"
+      redirect_to user_home_path
+    end
   end
 
-  private
-
-end
