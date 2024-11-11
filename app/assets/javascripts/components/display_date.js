0d5f022a67bdec9abb0e6054e1d55187757f7baa
mkljla <shazhidaye8@gmail.com>
2024-11-11 17:19:55 +0900

    [clean]日付と曜日のクラスを分離

--- app/assets/javascripts/components/display_date.js
+++ app/assets/javascripts/components/display_date.js
@@ -5,20 +5,26 @@
 // 初期表示
 displayDate();
 
-//1秒ごとに関数を呼び出す。
+// 1秒ごとに関数を呼び出す
 setInterval(displayDate, 1000);
 
 // 日付を表示する関数
 function displayDate() {
   const now = new Date();
-  const options = { year: "numeric", month: "long", day: "numeric", weekday: "long" };
-  const currentDate = now.toLocaleDateString("ja-JP", options);
+
+  // 日本の表記で日付と曜日をそれぞれ取得
+  const dateOptions = { year: "numeric", month: "2-digit", day: "2-digit" };
+  const weekdayOptions = { weekday: "short" };
+  const currentDate = now.toLocaleDateString("ja-JP", dateOptions);
+  const currentWeekday = now.toLocaleDateString("ja-JP", weekdayOptions);
 
   // HTML要素を取得
   const dateElement = document.querySelector(".date");
+  const weekdayElement = document.querySelector(".weekday");
 
-  // .date が存在する場合のみ時間を表示する
-  if (dateElement) {
-    dateElement.textContent = currentDate;
+  // 要素が存在する場合のみ表示を更新
+  if (dateElement && weekdayElement) {
+    dateElement.textContent = currentDate; // 日付部分
+    weekdayElement.textContent = `(${currentWeekday})`; // 曜日部分
   }
 }
