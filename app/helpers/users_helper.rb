9512eb9caf707fcc5ab54acc04b5d8e29814dd56
mkljla <shazhidaye8@gmail.com>
2024-11-22 14:58:14 +0900

    [update]共通メニューのレイアウト調整

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,14 @@
+module UsersHelper
+  def working_status_icon(working_status)
+    case working_status
+    when "working"
+      content_tag(:i, "", class: "bi bi-check-circle") # 勤務中のアイコン
+    when "breaking"
+      content_tag(:i, "", class: "bi bi-cup-hot") # 休憩中のアイコン
+    when "not_working"
+      content_tag(:i, "", class: "bi bi-house") # 勤務外のアイコン
+    else
+      content_tag(:i, "", class: "bi bi-question-circle") # 未登録のアイコン
+    end
+  end
+end
