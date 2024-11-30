5a548dea964d3f7a9b90f70d1de0fb0ac26490aa
mkljla <shazhidaye8@gmail.com>
2024-12-01 00:16:44 +0900

    [fix]パンくずリストの引数修正

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -8,7 +8,7 @@ end
 
 crumb :users_show do |user, admin_mode|
 
-  link "社員詳細", user_path(params[:id])
+  link "社員詳細(#{user.full_name})", user_path(user)
   if admin_mode
     parent :users_index
   else
@@ -18,7 +18,7 @@ end
 
 
 crumb :works_index  do |user, admin_mode|
-  link "勤務履歴",  user_works_path(user)
+  link "勤務履歴(#{user.full_name})",  user_works_path(user)
   if admin_mode
     parent :users_index
   else
@@ -26,9 +26,9 @@ crumb :works_index  do |user, admin_mode|
   end
 end
 
-crumb :works_show do |user, work|
+crumb :works_show do |user, work, admin_mode|
   link "勤務詳細", user_work_path(user, work)
-  parent :works_index
+  parent :works_index, user, admin_mode # 引数を渡す
 end
 
 
@@ -37,7 +37,7 @@ crumb :users_new do
   parent :users_index
 end
 
-crumb :users_edit do |user|
+crumb :users_edit do |user,  admin_mode|
   link "社員編集", edit_user_path(user)
-  parent :users_show
+  parent :users_show, user, admin_mode
 end
