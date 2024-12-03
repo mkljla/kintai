4c140b0c010483358e51d37ae14b885aca5ea289
mkljla <shazhidaye8@gmail.com>
2024-12-03 19:59:22 +0900

    [fix]ブラウザバックした際の不具合修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
     before_action :redirect_if_logged_in, only: [:new]
+    before_action :set_no_cache, only: [:new]
 
     def new
         # ログインフォームの表示
@@ -7,9 +8,10 @@ class SessionsController < ApplicationController
 
     # ログイン処理
     def create
-        reset_session # 古いセッションをクリア
         user = User.find_by(employee_number: params[:session][:employee_number].downcase)
         if user && user.authenticate(params[:session][:password])# パスワードの確認
+            reset_session # 古いセッションをクリア
+            form_authenticity_token # 新しいCSRFトークンを生成
             log_in user
             Rails.logger.info "Login successful for user: #{user.full_name} (Employee Number: #{user.employee_number})"
 
@@ -37,8 +39,14 @@ class SessionsController < ApplicationController
 
     def redirect_if_logged_in
         if logged_in?
-            flash[:notice] = "すでにログインしています。"
+            flash[:notice] = "ログイン済みです。"
             redirect_to home_users_path
         end
+    end
+
+    def set_no_cache
+        response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
     end
 end
