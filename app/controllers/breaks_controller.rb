492f88b50ee436d89b98d147d7f6710cd7ce5b3d
mkljla <shazhidaye8@gmail.com>
2024-12-03 15:17:36 +0900

    [fix]トランザクションの追加・修正

--- app/controllers/breaks_controller.rb
+++ app/controllers/breaks_controller.rb
@@ -4,52 +4,54 @@ class BreaksController < ApplicationController
   # 休憩開始処理
   def create
 
-    # 休憩記録の新規作成
-    break_record = Break.new(work_id: @latest_work.id, start_datetime: Time.current.change(sec: 0))
+    ActiveRecord::Base.transaction do
+      # 休憩記録の新規作成
+      break_record = Break.new(work_id: @latest_work.id, start_datetime: Time.current.change(sec: 0))
 
-    # 休憩記録の保存
-    if break_record.save
+      # 休憩記録の保存
+      break_record.save!
       # ステータスを休憩中に更新
       current_user.update!(working_status: :breaking)
-      flash[:notice] = "休憩を開始しました。"
-    else
-      flash[:alert] = "休憩時間の登録に失敗しました。"
     end
 
+    flash[:notice] = "休憩を開始しました。"
     redirect_to home_users_path
 
+  rescue StandardError => e
+    Rails.logger.error("Error during break creation: #{e.message}")
+    flash[:alert] = "休憩時間の登録に失敗しました。"
+    redirect_to home_users_path
   end
 
   def update
-    begin
-      ActiveRecord::Base.transaction do
-        # 休憩終了時間を登録
-        @latest_break.end_datetime = Time.current.change(sec: 0)
+    ActiveRecord::Base.transaction do
+      # 休憩終了時間を登録
+      @latest_break.end_datetime = Time.current.change(sec: 0)
 
-        # 休憩時間を計算
-        break_seconds = @latest_break.calculate_break_time
-        # 休憩時間を登録
-        @latest_break.break_time_in_minutes = (break_seconds / 60).to_i
+      # 休憩時間を計算
+      break_seconds = @latest_break.calculate_break_time
+      # 休憩時間を登録
+      @latest_break.break_time_in_minutes = (break_seconds / 60).to_i
 
-        raise ActiveRecord::Rollback unless @latest_break.save
+      raise ActiveRecord::Rollback unless @latest_break.save
 
-        # 累計休憩時間を登録
-        @latest_work.total_break_time_in_minutes = @latest_work.calculate_total_break_time
+      # 累計休憩時間を登録
+      @latest_work.total_break_time_in_minutes = @latest_work.calculate_total_break_time
 
-        raise ActiveRecord::Rollback unless @latest_work.save
-        # ステータスを勤務中に更新
-        raise ActiveRecord::Rollback unless current_user.update!(working_status: :working)
-        flash[:notice] = "休憩を終了しました。"
-        redirect_to home_users_path
-      end
-
-    rescue StandardError => e
-      # 例外処理
-      flash[:alert] = "休憩の終了に失敗しました: #{e.message}"
-      redirect_to home_users_path
+      @latest_work.save!
+      # ステータスを勤務中に更新
+      current_user.update!(working_status: :working)
     end
-  end
 
+    flash[:notice] = "休憩を終了しました。"
+    redirect_to home_users_path
+
+  rescue StandardError => e
+    # 例外処理
+    Rails.logger.error("Error during break update: #{e.message}")
+    flash[:alert] = "休憩の終了に失敗しました"
+    redirect_to home_users_path
+  end
 
 end
 
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -38,20 +38,22 @@ class WorksController < ApplicationController
 
   # 出勤処理
   def create
+    ActiveRecord::Base.transaction do
+      # 出勤記録の新規作成
+      work_record = Work.new(user_id: current_user.id, start_datetime: Time.current.change(sec: 0))
 
-    # 出勤記録の新規作成
-    work_record = Work.new(user_id: current_user.id, start_datetime: Time.current.change(sec: 0))
-
-    # 出勤記録の保存
-    if work_record.save
+      # 出勤記録の保存
+      work_record.save!
       # ステータスを勤務中に更新
       current_user.update!(working_status: :working)
-      flash[:notice] = "出勤時間を登録しました。"
-    else
-      flash[:alert] = "出勤時間の登録に失敗しました。"
     end
 
-    # ホーム画面にリダイレクト
+    flash[:notice] = "出勤時間を登録しました。"
+    redirect_to home_users_path
+
+  rescue StandardError => e
+    flash[:alert] = "出勤時間の登録に失敗しました。"
+    Rails.logger.error("Error during work creation: #{e.message}")
     redirect_to home_users_path
   end
 
@@ -59,34 +61,34 @@ class WorksController < ApplicationController
   # 退勤処理
   def update
 
-    begin
-      ActiveRecord::Base.transaction do
-        # 退勤記録の作成
-        @latest_work.end_datetime = Time.current.change(sec: 0)
+    ActiveRecord::Base.transaction do
+      # 退勤記録の作成
+      @latest_work.end_datetime = Time.current.change(sec: 0)
 
-        # 勤務時間(休憩含む)を計算
-        working_seconds = @latest_work.calculate_total_work_time
-        @latest_work.total_work_time_in_minutes = (working_seconds / 60).to_i
+      # 勤務時間(休憩含む)を計算
+      working_seconds = @latest_work.calculate_total_work_time
+      @latest_work.total_work_time_in_minutes = (working_seconds / 60).to_i
 
-        # 実労働時間を計算
-        @latest_work.actual_work_time_in_minutes = @latest_work.calculate_actual_work_time
-        # 残業時間を計算
-        @latest_work.total_overtime_in_minutes = @latest_work.calculate_overtime(MWorkHourSetting.standard_work_in_minutes)
-        # 保存
-        raise ActiveRecord::Rollback unless @latest_work.save
+      # 実労働時間を計算
+      @latest_work.actual_work_time_in_minutes = @latest_work.calculate_actual_work_time
+      # 残業時間を計算
+      @latest_work.total_overtime_in_minutes = @latest_work.calculate_overtime(MWorkHourSetting.standard_work_in_minutes)
 
-        # ステータスを勤務外に更新
-        current_user.update!(working_status: :not_working)
-
-        flash[:notice] = "退勤時間を登録しました。"
-        redirect_to home_users_path
-      end
-
-    rescue StandardError => e
-      # 例外処理
-      flash[:alert] = "休憩の終了に失敗しました: #{e.message}"
-      redirect_to home_users_path
+      # 保存
+      @latest_work.save!
+      # ステータスを勤務外に更新
+      current_user.update!(working_status: :not_working)
     end
+
+    flash[:notice] = "退勤時間を登録しました。"
+    redirect_to home_users_path
+
+
+  rescue StandardError => e
+    # 例外処理
+    flash[:alert] = "退勤時間の終了に失敗しました"
+    Rails.logger.error("Error during work update: #{e.message}")
+    redirect_to home_users_path
   end
 
 
