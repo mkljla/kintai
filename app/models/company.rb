f5f0781bafdf09ce5278281c1fec20231ffd976f
mkljla <shazhidaye8@gmail.com>
2024-12-31 15:24:16 +0900

    [upddate]companyモデルにバリデーション追加

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,9 @@
 class Company < ApplicationRecord
     has_many :users
+
+    # バリデーション
+    validates :name, :default_work_hours, presence: true
+    validates :name, length: { maximum: 50 }, format: { with: /\A[ぁ-んァ-ン一-龥a-zA-Z０-９Ａ-Ｚａ-ｚ]+\z/ }
+    validates :default_work_hours, format: { with: /\A\d+(\.\d{1})?\z/ }
+
 end
